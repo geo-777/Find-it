e117d3212ca359b7989bc476bcd5a1213ba976d0
geo-777 <geo.binu.25@gmail.com>
2026-03-12 08:35:08 +0530

    my-posts page empty state

--- pages/my-posts/my-posts.js
+++ pages/my-posts/my-posts.js
@@ -3,13 +3,15 @@ import {
   renderLoader,
   hideLoader,
 } from "../../shared/loader/loader-component.js";
-import { initAuth } from "../../shared/services/auth.js";
+import { initAuth, requireAuth } from "../../shared/services/auth.js";
+const app = document.getElementById("app");
 
 document.addEventListener("DOMContentLoaded", async () => {
   renderLoader();
   await initAuth();
 
   await renderNavbar();
-
+  requireAuth();
   hideLoader();
+  app.style.display = "block";
 });
